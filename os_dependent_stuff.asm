0b403114e8d657ac93854391a21cc1c5b52e7ad2
Rose Ames <rose@happyspork.com>
2014-03-07 14:57:45 -0500

    initial commit

---
+++ os_dependent_stuff.asm
@@ -0,0 +1,8 @@
+%ifidn __OUTPUT_FORMAT__,elf64
+%define SYSCALL_WRITE 1
+%define SYSCALL_EXIT 60
+%elifidn __OUTPUT_FORMAT__,macho64
+%define SYSCALL_WRITE 0x2000004
+%define SYSCALL_EXIT 0x2000001
+%endif
+
