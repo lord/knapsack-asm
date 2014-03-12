42126a93eee4c7ea34c462b56865745af4eb20c1
Robert Lord <robert@lord.io>
2014-03-12 16:42:37 -0400

    Update str_to_dec to use call and ret instead of jumps

--- knapsack.asm
+++ knapsack.asm
@@ -1,17 +1,16 @@
 %include "os_dependent_stuff.asm"
 
   mov rax, input_data
-  jmp str_to_dec
+  ; jmp str_to_dec
+  call str_to_dec
 
-program_end:
   mov rax, SYSCALL_EXIT
   mov rdi, rbx
   syscall
 
 input_data:
   ; db `4 11\n8 4\n10 5\n15 8\n4 3`, 0
-  db "142", 0
-
+  db "15", 0
 
 ; accept null terminated string pointed to by rax
 ; parses integer and puts into rbx
@@ -22,7 +21,7 @@ str_to_dec:
 str_to_dec_loop:
   mov cl, [rax]
   cmp rcx, 0
-  je str_to_dec_end
+  je return
   cmp rcx, 48
   jl str_to_dec_loop_end  ; if rcx < 60
   cmp rcx, 57
@@ -36,5 +35,5 @@ str_to_dec_loop_end:
   inc rax
   jmp str_to_dec_loop
 
-str_to_dec_end:
-  jmp program_end
+return:
+  ret
